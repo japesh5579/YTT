b2a923afa3da7b7a9eaff88a08b102da2d8feff7
japesh5579 <japeshjhatta634@gmail.com>
2026-08-17 00:54:36 +0530

    Remove Discover more link from Why Lighthouse YTT cards too

--- src/components/ExperienceCarousel.tsx
+++ src/components/ExperienceCarousel.tsx
@@ -49,7 +49,7 @@ export default function ExperienceCarousel({
             title={item.title}
             description={item.description}
             href={item.href}
-            showLink={variant !== "side"}
+            showLink={false}
             imgPositionClass={item.imgPositionClass}
             mediaClassName={`relative ${cardAspect} bg-[#e7e2d6]`}
             imgSize={{ w: 800, h: 800 }}
